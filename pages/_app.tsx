5be024d47677237078ff6bf96288a62b135ceb30
dac-liang <yunghsin-liang@dac.co.jp>
2022-04-01 16:50:47 +0900

    add auth logic use wagmi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ethers } from 'ethers';
+import { Provider as WagmiProvider } from 'wagmi';
 import '@fontsource/poppins';
+
+const provider = ethers.providers.getDefaultProvider('http://localhost:8545');
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <WagmiProvider autoConnect provider={provider}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </WagmiProvider>
   );
 }
 
